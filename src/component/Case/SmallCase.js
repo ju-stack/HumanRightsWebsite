b167fb59f6b99da24b893e41cd61ba1bb6966013
이준영 <110359963+ju-stack@users.noreply.github.com>
2023-11-29 13:58:20 +0000

    fourth

--- src/component/Case/SmallCase.js
+++ src/component/Case/SmallCase.js
@@ -1,6 +1,8 @@
 function SmallCase(props) {
+  console.log(props.alt)
     return (
       <div>
+        <img title={props.alt} alt={props.title} src={`img/${props.imgName}`}/>
         <h2>{props.title}</h2>
         <div>{props.story}</div>
       </div>

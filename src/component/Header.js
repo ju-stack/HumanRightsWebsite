f532d9ab4b448641f810ceefdca3dd0c4e30434b
이준영 <110359963+ju-stack@users.noreply.github.com>
2023-12-01 17:10:23 +0000

    fifth

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header>
-        <div>교권 존중 캠페인</div>
-        <nav>
-            <ul>
-                <li><Link to="/case">교권 침해 사례</Link></li>
-                <li><Link to="/respect">교권 존중 방법</Link></li>
-                <li><Link to="/quiz">교권 퀴즈</Link></li>
-            </ul>
-        </nav>
+        <h1>교권 존중 캠페인</h1>
+
+        <div className='navbar'>
+          <Link to="/case">교권 침해 사례</Link>
+          <Link to="/respect">교권 존중 방법</Link>
+          <Link to="/quiz">교권 퀴즈</Link>
+        </div>
     </header>
   );
 }
